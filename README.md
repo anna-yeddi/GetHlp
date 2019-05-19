553c76a0bafb4f314a0ed8050846a7ab62aa0af2
Anna Kova <m3ta007@gmail.com>
2019-05-19 17:40:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# GetHlp
+## Your Personal Assistant App
+
+Created in May 2019.
+
+## Features:
+- Mobile-first design;
+- Responsive layout;
+- SASS maps, functions, mixins etc.;
+- HTML5, CSS3 preprocessed via SASS (Dart) and packed with Grunt (incl. PostCSS & Uglify plugins).
---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    postcss: {
+      options: {
+        map: true, // inline sourcemaps
+        processors: [
+          require('pixrem')(), // add fallbacks for rem units
+          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+          require('cssnano')() // minify the result
+        ]
+      },
+      dist: {
+        src: 'css/*.css'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['postcss']);
+
+};
